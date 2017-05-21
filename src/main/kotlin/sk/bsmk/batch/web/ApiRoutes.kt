8575af02afc5d3edc11053076f9a73baa819afca
Miroslav Matejovsky <miroslav.matejovsky@gmail.com>
2017-05-21 11:06:26 +0200

    GET instances and executions

--- src/main/kotlin/sk/bsmk/batch/web/ApiRoutes.kt
+++ src/main/kotlin/sk/bsmk/batch/web/ApiRoutes.kt
@@ -7,10 +7,17 @@ import org.springframework.web.reactive.function.server.router
 @Configuration
 open class ApiRoutes(val apiHandler: ApiHandler) {
 
+  companion object {
+    const val JOB_NAME = "jobName"
+    const val INSTANCE_ID = "instanceId"
+  }
+
   @Bean
   open fun mainRouter() = router {
     GET("/", apiHandler::hello)
     GET("/jobs", apiHandler::jobs)
+    GET("/jobs/{$JOB_NAME}/instances", apiHandler::instances)
+    GET("/jobs/{$JOB_NAME}/instances/{$INSTANCE_ID}/executions", apiHandler::executions)
   }
 
 }
